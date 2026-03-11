9bf9e403a120cd445024593b981228ba379f76bc
Shyam Vaideeaswaran <shyamvaideeaswaran@gmail.com>
2026-03-11 18:54:50 +0000

    Added supabase.js

--- mobile-app/src/supabase.js
+++ mobile-app/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://qiztckqawtyuducthevs.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpenRja3Fhd3R5dWR1Y3RoZXZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MjY1MDgsImV4cCI6MjA4ODQwMjUwOH0.kA8E182O_ewmsHqAnaP1IO9FF9ILIAA2EarM_d9J6qg' 
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
